50d1a8fec347b141141ba994f3e708aece31392c
rtviwe <rtviwe@gmail.com>
2018-07-16 18:59:55 +0300

    First way for cards

--- app/src/main/java/rtviwe/com/retabelo/main/fragments/recommendations/RecommendationsAdapter.kt
+++ app/src/main/java/rtviwe/com/retabelo/main/fragments/recommendations/RecommendationsAdapter.kt
@@ -1,26 +1,26 @@
 package rtviwe.com.retabelo.main.fragments.recommendations
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.firebase.ui.firestore.paging.FirestorePagingAdapter
 import com.firebase.ui.firestore.paging.FirestorePagingOptions
+import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.recommendation_list_item.view.*
 import rtviwe.com.retabelo.R
 import rtviwe.com.retabelo.database.recipe.Recipe
 
-class RecommendationsAdapter(options: FirestorePagingOptions<Recipe>)
+class RecommendationsAdapter(private val context: Context,
+                             options: FirestorePagingOptions<Recipe>)
     : FirestorePagingAdapter<Recipe, RecommendationsAdapter.RecommendationsViewHolder>(options) {
-
-    private var parentView: ViewGroup? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecommendationsViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.recommendation_list_item,
                 parent, false)
-
-        parentView = parent
-
         return RecommendationsViewHolder(view)
     }
 
@@ -32,9 +32,24 @@ class RecommendationsAdapter(options: FirestorePagingOptions<Recipe>)
 
         private var nameTextView = itemView.text_view_recommended_dish
         private var photoImageView = itemView.image_view_recommended_dish
+        private var bodyTextView = itemView.text_view_recommended_body
+
+        private val firebaseStorage = FirebaseStorage.getInstance()
+        private val firebaseStoragePhotos = firebaseStorage.reference.child("photos")
 
         fun bindTo(item: Recipe) {
             nameTextView.text = item.name
+            bodyTextView.text = item.body.take(509).plus("...")
+
+            val storageReferenceToPhoto = firebaseStoragePhotos.child(item.photo)
+
+            storageReferenceToPhoto.downloadUrl.addOnSuccessListener {
+                Picasso.with(context)
+                        .load(it)
+                        .into(photoImageView)
+            }.addOnFailureListener {
+                Log.e("RecommendationsAdapter", it.stackTrace.toString())
+            }
         }
     }
 }
--- app/src/main/java/rtviwe/com/retabelo/main/fragments/recommendations/RecommendationsFragment.kt
+++ app/src/main/java/rtviwe/com/retabelo/main/fragments/recommendations/RecommendationsFragment.kt
@@ -2,13 +2,10 @@ package rtviwe.com.retabelo.main.fragments.recommendations
 
 import android.arch.paging.PagedList
 import android.os.Bundle
-import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import com.firebase.ui.firestore.paging.FirestorePagingOptions
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.recommendations_fragment.*
 import rtviwe.com.retabelo.R
 import rtviwe.com.retabelo.database.recipe.Recipe
@@ -19,18 +16,11 @@ class RecommendationsFragment : BaseFragment() {
 
     override val layoutId = R.layout.recommendations_fragment
 
-    private lateinit var firebaseFirestore: FirebaseFirestore
-    private lateinit var firebaseStorage: FirebaseStorage
-    private lateinit var firebasePhotosReference: StorageReference
-
+    private val firebaseFirestore = FirebaseFirestore.getInstance()
     private lateinit var recommendationsAdapter: RecommendationsAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        firebaseFirestore = FirebaseFirestore.getInstance()
-        firebaseStorage = FirebaseStorage.getInstance()
-        firebasePhotosReference = firebaseStorage.reference.child("photos")
 
         initAdapter()
         initRecyclerView()
@@ -50,13 +40,11 @@ class RecommendationsFragment : BaseFragment() {
             setQuery(queryForRecipes, config, Recipe::class.java)
         }.build()
 
-        recommendationsAdapter = RecommendationsAdapter(options)
+        recommendationsAdapter = RecommendationsAdapter(context!!, options)
     }
 
     private fun initRecyclerView() {
         recycler_view_recommendations.apply {
-            addItemDecoration(DividerItemDecoration(activity?.applicationContext,
-                    DividerItemDecoration.VERTICAL))
             layoutManager = LinearLayoutManager(context)
             adapter = recommendationsAdapter
         }
